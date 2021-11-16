587f92599c1c347939f0f936443a162cf575b6be
RAMARJUN397 <69568813+RAMARJUN397@users.noreply.github.com>
2021-11-16 12:04:41 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  backend "s3" {
+    bucket = "Ramarjun"
+    key = "backend.tf"
+    region = "ap-south-1"
+    
+  }
+}
+
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+  #access_key = "AKIAUZBJNUI7NTZDR357"
+  #secret_key = "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"
+
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "Ramarjun"
+  acl    = "private"
+
+  tags = {
+    Name        = "Ramarjun"
+    Environment = "OPS"
+  }
+}
