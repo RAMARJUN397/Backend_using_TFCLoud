c24b7aa6dde47d3b8f003cb7bd32700b381d997a
RAMARJUN397 <69568813+RAMARJUN397@users.noreply.github.com>
2021-11-16 12:41:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,19 +11,10 @@ terraform {
 
 
 # Configure the AWS Provider
-provider "aws" {
-  region = "ap-south-1"
-  access_key = "AKIAUZBJNUI7NTZDR357"
-  secret_key = "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"
+#provider "aws" {
+  #region = "ap-south-1"
+  #access_key = "AKIAUZBJNUI7NTZDR357"
+  #secret_key = "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"
 
-}
+#}
 
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "remodemo"
-  acl    = "private"
-
-  tags = {
-    Name        = "remodemo"
-    Environment = "OPS"
-  }
-}
